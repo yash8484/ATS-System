757e842af2e7a08d0104f0a56c6cdbe726ab250e
Yash Sontakke <75352487+yash8484@users.noreply.github.com>
2025-02-20 10:03:05 +0530

    Update app1.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 import PyPDF2 as pdf
 from dotenv import load_dotenv
-
+import json
 
 load_dotenv() ## load all our environment variables
 
@@ -23,25 +23,20 @@ def input_pdf_text(uploaded_file):
     return text
 
 #Prompt Template
+
 input_prompt="""
-Hey Act Like a skilled or very experienced ATS(Application Tracking System)
-with a deep understanding of the tech field, software engineering, data science, data analysis
-and big data engineering. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide the 
-best assistance for improving their resumes. Assign the percentage Matching based 
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving thr resumes. Assign the percentage Matching based 
 on Jd and
 the missing keywords with high accuracy
 resume:{text}
 description:{jd}
 
-I want the response in this structure
-"JD Match":"%",
-
-"MissingKeywords:[]",
-
-"Profile Summary":""
-
-
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
 """
 
 ## streamlit app
